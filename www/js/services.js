56da88ec6bdb2ecb5fb9cd87ab20e967cc8eb9cc
Roussel Kamaha <kamaha12000@yahoo.fr>
2017-01-09 12:42:33 -0500

    We implemented the code to unable the favorite options to work. So a user can now add dishes to its favorite. Also using features of ionic list, we allow the user to be able to quickly remove dishes from its favorite. a custom filter was implemented to allow favorites dishes to be filter from the rest of dishes. a favorite template was created and a link to this template was added to the sidebar menu.

--- www/js/services.js
+++ www/js/services.js
@@ -45,19 +45,31 @@ angular.module('conFusion.services', ['ngResource'])
     
         }])
 
-        .factory('FavoriteFactory', ['resource', 'baseURL', function($resource, baseURL){
+     .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
           var favFac = {};
           var favorites = [];
 
-          favFac.addToFavorites = function(index){
-            for (var i=0; i < favorites.length; i++){
-              if(favorites[i].id == index)
+        favFac.addToFavorites = function (index) {
+          for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index)
                 return;
-            }
+         }
+          favorites.push({id: index});
+        };
 
-            favorites.push({id: index});
-          };
-          return favFac;
-        }])
+        favFac.deleteFromFavorites = function (index) {
+        for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index) {
+                favorites.splice(i, 1);
+            }
+         }
+       }
+
+      favFac.getFavorites = function () {
+        return favorites;
+      };
+
+    return favFac;
+    }])
 
 ;
